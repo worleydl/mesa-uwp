7536743dace1566497c1ea4d7f7ae21e4ea4a7cb
Tomeu Vizoso <tomeu.vizoso@ideasonboard.com>
2024-11-06 10:26:20 +0100

    etnaviv/nn: Fix use of etna_core_info
    Right now we were retrieving the properties of the NPU from the
    etna_core_info of the GPU.
    
    Fixes: 92a6f697d589 ("etnaviv: npu: Switch to use etna_core_info")
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
    (cherry picked from commit f9bb9aa7d5ee87069c65ccef26e0f762dadce597)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32119>

--- src/gallium/drivers/etnaviv/etnaviv_ml.h
+++ src/gallium/drivers/etnaviv/etnaviv_ml.h
@@ -129,6 +129,8 @@ struct pipe_resource *etna_ml_create_resource(struct pipe_context *pctx, size_t 
 
 struct etna_core_npu_info *etna_ml_get_core_info(struct etna_context *context);
 
+struct etna_core_npu_info *etna_ml_get_core_info(struct etna_context *context);
+
 struct pipe_ml_subgraph *
 etna_ml_subgraph_create(struct pipe_context *context,
                         const struct pipe_ml_operation *operations,
